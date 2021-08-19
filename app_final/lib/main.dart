87382369e19aeb3f6062ef44382b49986111d65a
kathrd <katherine03gt@gmail.com>
2021-08-18 20:39:43 -0400

    creando el proyecto

---
+++ app_final/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:app_final/pages/splash-page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashPage(),
+    );
+  }
+}
